8a9d286d33b3a403d02a330ea50e6a7b8c2a8c00
Efosa Uwa-Omede <euwaomed@nyit.edu>
2018-02-26 18:02:05 -0500

    First Commit - Needs Descriptions added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,47 @@
+import java.util.Scanner;
+
 public class Calculator
 {
+    static void project03()
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+
+        while(true)
+        {
+            System.out.println("enter a message");
+            String line = keyboard.nextLine();
+            System.out.println(line);
+            String[] array = line.split("[ |,]");
+            String cmd = array[0];
+
+            int a=-1, b=-1;
+
+            if(array.length == 3)
+            {
+                a = Integer.parseInt(array[1]);
+                b = Integer.parseInt(array[2]);
+            }
+
+//        if(cmd.equals("add"))
+//            System.out.println(a + b);
+//        else if(cmd.equals("sub"))
+//            System.out.println(a - b);
+//        else
+
+            switch (cmd)
+            {
+                case "add": System.out.println(a+b); break;
+                case "sub": System.out.println(a-b); break;
+                case "mul": System.out.println(a*b); break;
+                case "div": System.out.println(a/b); break;
+                case "exit": System.exit(0);
+            }
+        }
+    }
+
     public static void main(String[] args)
     {
-
+        project03();
     }
 }
