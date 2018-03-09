b5d6d65d27df33f0a7f8f7141de6dabc5cf6b20d
Efosa Uwa-Omede <35854416+euwaomed@users.noreply.github.com>
2018-03-09 17:40:57 -0500

    Update Calculator.Java
    Fixed for testrunner

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,47 +1,43 @@
 import java.util.Scanner;
-
-public class Calculator
-{
-    static void project03()
-    {
+public class Calculator {
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
+            /*while (keyboard.hasNextLine()) {
+               // System.out.println("enter a message");
+                String line = keyboard.nextLine();
+                // System.out.println(line);
+                String[] array = line.split(" ");
+                String cmd = array[0]; */
 
-        while(true)
-        {
-            System.out.println("enter a message");
+        while (keyboard.hasNextLine()) {
             String line = keyboard.nextLine();
-            System.out.println(line);
-            String[] array = line.split("[ |,]");
+            String[] array = line.split(" ");
             String cmd = array[0];
 
-            int a=-1, b=-1;
+                int a = -1, b = -1;
 
-            if(array.length == 3)
-            {
-                a = Integer.parseInt(array[1]);
-                b = Integer.parseInt(array[2]);
-            }
+                if (array.length == 3) {
+                    a = Integer.parseInt(array[1]);
+                    b = Integer.parseInt(array[2]);
 
-//        if(cmd.equals("add"))
-//            System.out.println(a + b);
-//        else if(cmd.equals("sub"))
-//            System.out.println(a - b);
-//        else
-
-            switch (cmd)
-            {
-                case "add": System.out.println(a+b); break;
-                case "sub": System.out.println(a-b); break;
-                case "mul": System.out.println(a*b); break;
-                case "div": System.out.println(a/b); break;
-                case "exit": System.exit(0);
+                    switch (cmd) {
+                        case "add":
+                            System.out.println(a + b);
+                            break;
+                        case "sub":
+                            System.out.println(a - b);
+                            break;
+                        case "mul":
+                            System.out.println(a * b);
+                            break;
+                        case "div":
+                            System.out.println(a / b);
+                            break;
+                        case "exit":
+                            System.exit(0);
+                    }
+                }
             }
         }
     }
-
-    public static void main(String[] args)
-    {
-        project03();
-    }
-}
